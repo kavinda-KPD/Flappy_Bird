2f1200782a83ce9a57c81ec7dc3d04c4432f13bb
kavinda-KPD <kavindadissanayake1999@gmail.com>
2025-06-20 16:26:57 +0530

    Implement pipe pool

--- assets/Scripts/PipePool.ts
+++ assets/Scripts/PipePool.ts
@@ -1,14 +1,53 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, instantiate, Node, NodePool, Prefab } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('PipePool')
+@ccclass("PipePool")
 export class PipePool extends Component {
-    start() {
+  @property({
+    type: Prefab,
+    tooltip: "Pipe",
+  })
+  public preFabPipe: Prefab = null;
 
+  @property({
+    type: Node,
+  })
+  pipePoolHome: Node = null;
+
+  public pool = new NodePool();
+  public createPipe;
+
+  initPool() {
+    let initCount = 3;
+
+    for (let i = 0; i < initCount; i++) {
+      this.createPipe = instantiate(this.preFabPipe);
+
+      if (i === 0) {
+        this.pipePoolHome.addChild(this.createPipe);
+      } else {
+        this.pool.put(this.createPipe);
+      }
+    }
+  }
+
+  addPool() {
+    if (this.pool.size() > 0) {
+      this.createPipe = this.pool.get();
+    } else {
+      this.createPipe = instantiate(this.preFabPipe);
     }
 
-    update(deltaTime: number) {
-        
-    }
+    this.pipePoolHome.addChild(this.createPipe);
+  }
+
+  resetPool() {
+    this.pipePoolHome.removeAllChildren();
+    this.pool.clear();
+    this.initPool();
+  }
+
+  start() {}
+
+  update(deltaTime: number) {}
 }
-
